d5f4c7967e0f7d6f47f2ae69304c6a07e7e9e39a
Jedsadaporn pannok <tikpop555@gmail.com>
2025-05-06 21:28:01 +0700

    preload

--- internal/models/airport/airportpreload.go
+++ internal/models/airport/airportpreload.go
@@ -1,11 +1,14 @@
 package airport
 
 type AirportPreload struct {
-	ID       uint   `gorm:"column:airport_id;primaryKey" json:"airport_id"`
-	IATACode string `gorm:"column:iata_code" json:"iata_code"`
-	Name     string `json:"name"`
-	City     string `json:"city"`
-	Country  string `json:"country"`
+	ID        uint    `gorm:"column:airport_id;primaryKey" json:"airport_id"`
+	IATACode  string  `gorm:"column:iata_code" json:"iata_code"`
+	Name      string  `json:"name"`
+	City      string  `json:"city"`
+	Country   string  `json:"country"`
+	Latitude  float64 `json:"latitude"`
+	Longitude float64 `json:"longitude"`
+	Timezone  string  `json:"timezone"`
 }
 
 func (AirportPreload) TableName() string {
